54fb9a600b4863043067e5d6d5738ba9e9b856f1
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-04-07 15:47:08 +0200

    Create Main.java

---
+++ Actividades/Act_Abstract/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Coche coches = new Coche();
+        coches.SonidoV();
+        coches.motor();
+        coches.CerrarPuerta();
+    }
+}
