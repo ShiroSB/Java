f7a184d23c6f545efad19dfb58d485f404ae64de
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-04-07 15:48:15 +0200

    Create Vehiculos.java

---
+++ Actividades/Act_Abstract/Vehiculos.java
@@ -0,0 +1,11 @@
+package com.company;
+
+abstract class Vehiculos {
+    public abstract void SonidoV();
+
+    public abstract void CerrarPuerta();
+
+    public void motor(){
+        System.out.println("Brbrbr");
+    }
+}
