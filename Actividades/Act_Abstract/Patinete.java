5054f1bcd5b2a326fa81ce2382aacecac4a6e96f
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-04-07 16:05:00 +0200

    Create Patinete.java

---
+++ Actividades/Act_Abstract/Patinete.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Patinete extends Vehiculos {
+
+    @Override
+    public void SonidoV() {
+        System.out.println("Patineta brr");
+    }
+    @Override
+    public void CerrarPuerta(){
+        System.out.println("A");
+    }
+
+}
