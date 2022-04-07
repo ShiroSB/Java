035b404edafb67889b902b57e91bbec7a8f25428
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-04-07 15:47:51 +0200

    Create Coche.java

---
+++ Actividades/Act_Abstract/Coche.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Coche extends Vehiculos {
+
+    @Override
+    public void SonidoV() {
+        System.out.println("Rrrrrrrrr");
+    }
+    @Override
+    public void CerrarPuerta(){
+        System.out.println("Bum");
+    }
+}
