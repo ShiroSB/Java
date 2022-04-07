bf1b15763f5a53a693333351c54b2763fc2d11fc
ShiroSB <95426128+ShiroSB@users.noreply.github.com>
2022-04-07 16:03:45 +0200

    Update Coche.java

--- Actividades/Act_Abstract/Coche.java
+++ Actividades/Act_Abstract/Coche.java
@@ -1,15 +1,13 @@
 package com.company;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Coche coches = new Coche();
-        coches.SonidoV();
-        coches.CerrarPuerta();
-        coches.motor();
 
-        Patinete patinetes = new Patinete();
-        patinetes.SonidoV();
-        patinetes.CerrarPuerta();
-        patinetes.motor();
+public class Coche extends Vehiculos {
+
+    @Override
+    public void SonidoV() {
+        System.out.println("Rrrrrrrrr");
+    }
+    @Override
+    public void CerrarPuerta(){
+        System.out.println("Bum");
     }
 }
